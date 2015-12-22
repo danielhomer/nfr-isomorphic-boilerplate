34a98ea0c712688fe16a597630e65386f3a7aee2
Daniel Homer <daniel@Daniels-MacBook-Pro.local>
2015-12-22 16:48:40 +0000

    Tabs to spaces

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 class Home extends React.Component {
 
-	render() {
-		return (
-			<div className='alert alert-info'>
-				Hello from Home Component
-			</div>
-			);
-	}
+  render() {
+    return (
+      <div className='alert alert-info'>
+        Hello from Home Component
+      </div>
+      );
+  }
 
 }
 
